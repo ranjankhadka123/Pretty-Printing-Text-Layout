9a34985a51f08a9a8d82a0b859c60c90d807b882
Demo User <ranjan.khadka@bison.howard.edu>
2017-06-28 23:42:46 -0700

    fix some errors

--- lab7/src/lab7/TextLayout.java
+++ lab7/src/lab7/TextLayout.java
@@ -7,15 +7,17 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 
 public class TextLayout {
 
     // Layout text.
     // Lay out the text contained in the List<String> words into the individual lines.
     private static List<String> layoutText(List<String> words, int maxLineWidth) {
-	return greedyLayoutText(words, maxLineWidth);
+	//return greedyLayoutText(words, maxLineWidth);
 	// TODO: Uncomment this after implementing prettyPrintLayoutText()
-	// return prettyPrintLayoutText(words, maxLineWidth);
+	return prettyPrintLayoutText(words, maxLineWidth);
     }
 
     // Layout the text in the minimal number of lines.
@@ -40,35 +42,83 @@ public class TextLayout {
 		
 	}
 	lines.add(string.substring(0, string.length()-1));
-	System.out.println("Total Cost : " + cost_function(lines, maxLineWidth));
+	//System.out.println("Total Cost : " + cost_function(lines, maxLineWidth));
 	return lines;
     }
 
     // Layout the text minimizing the "pretty print" cost function.
     private static List<String> prettyPrintLayoutText(List<String> words, int maxLineWidth) {
-	List<String> lines = new ArrayList<String>();
-	Map<Integer, Integer> id_cost = new Hashmap<String, Integer>();
-	Map<Integer, Integer> id_numLine = new Hashmap<String, Integer>();
-	int i = words.size()-1;
-	String string = "";
-	int count = 0;
-	while(i > 0) {
-		if(string.length() <= maxLineWidth){
-			string = string + words.get(i);
-			int cost = (maxLineWidth - string.length()) * (maxLineWidth - string.length());
-			id_cost.put(string, cost);
-			count ++;
-			id_numline(string, count);
-			string = string + " ";
-			i++;
-		} else {
-			for( String key : id_cost.keySet()){
-
+		List<String> lines = new ArrayList<String>();
+		Map<Integer, Integer> id_cost = new HashMap<Integer, Integer>();
+		Map<Integer, Integer> id_numLine = new HashMap<Integer, Integer>();
+		int index = words.size()-1;
+	    String string = "";
+		int count = 0;
+		int size = 0;
+		int m = 0;
+		int j = 0;
+		String temp = "";
+		while(index >= 0){
+			temp = words.get(index);
+			if(size <= maxLineWidth) {
+				string = string + words.get(index) + " ";
+				count++;
+				size = string.length();
+				temp = "";
+				index--;
 			}
 		}
-	}
+		id_cost.put(m, 0);
+		id_numLine.put(j, count);
+		int min = Integer.MAX_VALUE;
+		size = 0;
+		string ="";
+		int total_cost = 0;
+		count = 0;
+		int word_count = 0;
+		while(index >= 0){
+			temp = words.get(index);
+			if(size + temp.length() <= maxLineWidth){
+				string = string + words.get(index);
+				total_cost = total_cost + (maxLineWidth - string.length()) * (maxLineWidth - string.length());
+				count++;
+				if(total_cost < min ){
+					min = total_cost;
+					word_count = count;
+				}
+				string = string + " ";
+				size = string.length();
+				temp = "";
+				index--;
 
-	return lines;
+			} else {
+				id_cost.put(m++, min);
+				id_numLine.put(j++, word_count);
+				string = "";
+				size = 0;
+				min = Integer.MAX_VALUE;
+				total_cost = 0;
+				count = 0;
+			}
+
+		
+		}
+		id_cost.put(m++, min);
+		id_numLine.put(j++, word_count);
+		String line = "";
+		for(int i = 0; i < words.size(); i++){
+			int no_of_words = 0;
+			if(no_of_words <= id_numLine.get(j)){
+				line = words.get(i) + " ";
+				no_of_words++;
+			} else {
+				lines.add(line.substring(0, string.length()-1));
+				j = j - id_numLine.get(j);
+				string = "";
+			}
+
+		}
+		return lines;
     }
 
     public static void main(String[] args) {
